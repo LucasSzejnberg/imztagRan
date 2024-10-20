79e1bc4efc4428f5b849f08547b10516c3615948
Lucasszejn <127147761+LucasSzejnberg@users.noreply.github.com>
2024-10-20 17:48:31 -0300

    vercel.jos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import InstagramLogin from './Instagram';
 import VerBase from './VerBase.tsx';  // Nuevo componente
@@ -6,14 +6,10 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-
     <Routes>
       <Route path="/login" element={<InstagramLogin />} />
       <Route path="/verbase" element={<VerBase />} />
     </Routes>
-    </Router>
-
   );
 }
 
