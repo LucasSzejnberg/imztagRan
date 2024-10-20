5d0c9fc94c571038e774ac5a06754330a309a8e7
Lucasszejn <127147761+LucasSzejnberg@users.noreply.github.com>
2024-10-20 16:05:53 -0300

    te pego

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import InstagramLogin from './Instagram';
 import VerBase from './VerBase.tsx';  // Nuevo componente
 import './App.css';
 
 function App() {
   return (
+    <Router>
+
     <Routes>
       <Route path="/login" element={<InstagramLogin />} />
       <Route path="/verbase" element={<VerBase />} />
     </Routes>
+    </Router>
+
   );
 }
 
